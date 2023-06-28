d00f81b53d7f7459cbf4cc4927d0f7b4cd2cb13d
Noah Cowan <nsncowan@gmail.com>
2023-06-28 15:06:10 -0700

    begin TimeBlockControl, add firebase,  .gitignore

---
+++ src/components/TimeBlockControl.js
@@ -0,0 +1,18 @@
+import React, { useState } from "react";
+import NewTimeBlockForm from "./NewTImeBlockForm";
+import TimeBlockList from "./TimeBlockList";
+
+
+function TimeBlockControl() {
+  const [timeBlockList, setTimeBlockList] = useState([]);
+  const [selectedTimeBlock, setSelectedTimeBlock] = useState(null);
+  const [editing, setEditing] = useState(false);
+
+  
+
+
+
+
+}
+
+export default TimeBlockControl;
