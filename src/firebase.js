080d6469a81668dda284acbe05090611594f5d1e
Noah Cowan <nsncowan@gmail.com>
2023-06-28 17:30:35 -0700

    create firebase.js

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+/* This is where we'll initialize Firebase in our application and create a database reference. 
+Note that Firebase is not prescriptive about what we name our configuration file or where we located, 
+so it's up to us to pick a name and location that is sensible and intuitive. */
+
+import { initializeApp } from "firebase/app";
+import { getFirestore } from 'firebase/firestore';
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID 
+}
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+const db = getFirestore(app);
+
+export { db, auth }; // I initially had 'default' included in this line, but it was throwing an error. idk why yet.
