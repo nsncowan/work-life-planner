253feb91a1a309470dec3c02d1d7c16581386adf
Noah Cowan <nsncowan@gmail.com>
2023-07-09 15:52:17 -0700

    comment out DayView

--- src/components/App.js
+++ src/components/App.js
@@ -23,7 +23,7 @@ function App(){
       <React.Fragment>
         <Header />
         <TimeBlockControl />
-        <DayView />
+        {/* <DayView /> */}
       </React.Fragment>
     </div>
   );
