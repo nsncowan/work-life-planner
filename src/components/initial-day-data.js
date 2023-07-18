f74cb5d3bf5f4de4e37d5f9fea55d1b5572737d3
Noah Cowan <nsncowan@gmail.com>
2023-07-18 16:06:48 -0700

    shorten initial day data

--- src/components/initial-day-data.js
+++ src/components/initial-day-data.js
@@ -3,30 +3,6 @@ import { v4 } from 'uuid';
 const initialDayData = [
   // add date property
   {
-    time: "6:00am",
-    name: null,
-    category: null,
-    id: v4(),
-  },
-  {
-    time: "7:00am",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "8:00am",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "9:00am",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
     time: "10:00am",
     name: null,
     category: null,
@@ -58,54 +34,6 @@ const initialDayData = [
   },
   {
     time: "3:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "4:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "5:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "6:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "7:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "8:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "9:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "10:00pm",
-    name: null,
-    category: null,
-    id: v4()
-  },
-  {
-    time: "11:00pm",
     name: null,
     category: null,
     id: v4()
