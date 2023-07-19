b9365e8cd258e9c2f36be1cbcbff5fd46a3745ba
Noah Cowan <nsncowan@gmail.com>
2023-07-18 17:49:22 -0700

    save

--- src/components/initial-day-data.js
+++ src/components/initial-day-data.js
@@ -4,38 +4,38 @@ const initialDayData = [
   // add date property
   {
     time: "10:00am",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
   {
     time: "11:00am",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
   {
     time: "12:00pm",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
   {
     time: "1:00pm",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
   {
     time: "2:00pm",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
   {
     time: "3:00pm",
-    name: null,
-    category: null,
+    // name: null,
+    // category: null,
     id: v4()
   },
 ];
