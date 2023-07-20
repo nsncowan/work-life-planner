686867289b6004ad5c5bc0d0456547e2737dec9a
Noah Cowan <nsncowan@gmail.com>
2023-07-20 14:53:51 -0700

    increment day functionality

--- src/components/SelectDate.js
+++ src/components/SelectDate.js
@@ -1,22 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { format, addDays } from "date-fns";
+import { format, addDays, eachDayOfInterval, startOfToday, startOfMonth, endOfMonth, parseISO, parse, add} from "date-fns";
 
 function SelectDate() {
-  const [displayedDate, setDisplayedDate] = useState(new Date());
-  let formattedDate = format(new Date(), 'MM-dd-yyyy');
+  // console.log('today: ', today);
+  // console.log('julyArray: ', julyArray);
+  
+  
+  let today = startOfToday();
+  const [displayedDate, setDisplayedDate] = useState(today);
+  let julyArray = eachDayOfInterval({ start: startOfMonth(today), end: endOfMonth(today) });
+  let formattedDate = format(today, 'MM-dd-yyyy');
+  
+  let [currentDay, setCurrentDay] = useState(format(today, 'MM-dd-yyyy'))
+  console.log(currentDay);
 
-  function advanceDate(e){
-    e.preventDefault();
-    const newDate = addDays(formattedDate, 1);
-    setDisplayedDate(newDate);
-    // return formattedDate;
-  };
+  function nextDay() {
+    let newDay = parse(currentDay, 'MM-dd-yyyy', new Date());
+    let nextDay = add(newDay, { days: 1})
+    setCurrentDay(format(nextDay, 'MM-dd-yyyy'))
+  }
+  
+  
+  // console.log('formattedDate: ', formattedDate);
+  // console.log('displayedDate: ', displayedDate);
+
+  // const advanceDate = () => {
+  //   const newDate = addDays(parseISO(formattedDate), 1);
+  //   setDisplayedDate(newDate);
+  // };
 
   return(
     <div>
-      <button>back</button>
-      <h3>{displayedDate}</h3>
-      <button onClick={advanceDate}>forward</button>
+      <button type="button">back</button>
+      <h3>{currentDay}</h3>
+      <button type="button" onClick={nextDay}>forward</button>
     </div>
   );
 }
